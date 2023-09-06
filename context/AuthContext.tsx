768787a1cea1e735fdcb832b4da187f8727f7111
jinwood <jinw@protonmail.com>
2023-09-06 11:42:58 +0100

    Configure auth

---
+++ context/AuthContext.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { onAuthStateChanged, getAuth, User } from "firebase/auth";
+import firebase_app from "@/firebase/config";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+interface Props {
+  user: User | null;
+}
+
+const auth = getAuth(firebase_app);
+
+export const AuthContext = createContext<Props | undefined>(undefined);
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error(
+      "useAuthContext must be used within an AuthContextProvider"
+    );
+  }
+  return context;
+};
+
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {loading ? <div>Loading...</div> : children}
+    </AuthContext.Provider>
+  );
+};
