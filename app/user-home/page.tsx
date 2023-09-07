47993239add82ad81b7af25f4574cb8f061fc010
jinwood <jinw@protonmail.com>
2023-09-07 16:54:02 +0100

    Logout

--- app/user-home/page.tsx
+++ app/user-home/page.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
+import logout from "@/firebase/auth/logout";
 function Page() {
   const { user } = useAuthContext();
   const router = useRouter();
@@ -10,7 +11,16 @@ function Page() {
     if (user == null) router.push("/");
   }, [user, router]);
 
-  return <h1>Only logged in users can view this page</h1>;
+  const handleLogout = async () => {
+    await logout();
+  };
+
+  return (
+    <>
+      <p>You are logged in as {user?.email}</p>
+      <button onClick={handleLogout}>Logout</button>
+    </>
+  );
 }
 
 export default Page;
